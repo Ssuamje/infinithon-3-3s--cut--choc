7caed2d1b27fccacf7152ccc766892cfc0326594
Park323 <park32323@gmail.com>
2025-08-09 18:01:15 +0900

    update proto

--- server/main.py
+++ server/main.py
@@ -9,8 +9,11 @@ import uuid
 app = FastAPI()
 
 # 데이터 모델 정의
-class BlinkData(BaseModel):
-    timestamp: list[str]  # 타임스탬프 리스트
+class BlinkSession(BaseModel):
+    id: str
+    events: list[str]  # 타임스탬프 리스트
+    startedAt: str
+    endedAt: str
 
 # 요청 데이터를 저장할 임시 메모리 저장소 (예: 데이터베이스 대신 사용)
 data_store: Dict[str, Dict] = {}
@@ -26,11 +29,10 @@ async def cleanup_data_store():
 
 # 클라이언트 → 서버: 데이터 수신
 @app.post("/blink-data/")
-async def receive_blink_data(data: BlinkData):
-    request_id = str(uuid.uuid4())  # 난수로 고유 ID 생성
+async def receive_blink_data(data: BlinkSession):
     timestamp = time.time()
-    data_store[request_id] = data.timestamp
-    return {"message": "Data received and processed successfully", "request_id": request_id, "timestamp": timestamp}
+    data_store[data.id] = data.events
+    return {"message": "Data received and processed successfully", "id": data.id, "timestamp": timestamp}
 
 # 서버 → 클라이언트: 가공된 데이터 전송
 @app.get("/processed-data/{request_id}")
@@ -40,4 +42,4 @@ async def send_processed_data(request_id: str):
     # processed_data = analyze_tablet_data(data_store[request_id]['state'])
     # report = generate_report(processed_data)
     report = data_store[request_id]
-    return {"report": report, "request_id": request_id}
+    return {"report": report}
